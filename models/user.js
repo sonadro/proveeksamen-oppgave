5782ab1f6d6eea4c57465c669cb6eaf1bcc917e4
sonadro <sondre.rooth@gmail.com>
2023-05-04 13:10:19 +0200

    Mongoose modell for brukere

---
+++ models/user.js
@@ -0,0 +1,29 @@
+// packages
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    posted_blogs: {
+        type: Array,
+        required: false
+    },
+    posted_comments: {
+        type: Array,
+        required: false
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
