84cbf88a0a0163c7752d8d18aaef562715fff416
sonadro <sondre.rooth@gmail.com>
2023-05-09 08:43:09 +0200

    Logg inn & lag bruker sider

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -6,3 +6,11 @@ module.exports.home_get = (req, res) => {
 module.exports.blogger_get = (req, res) => {
     res.render('blogger', { title: 'Blogger' });
 };
+
+module.exports.login_get = (req, res) => {
+    res.render('logg-inn', { title: 'Logg inn' });
+};
+
+module.exports.create_get = (req, res) => {
+    res.render('lag-bruker', { title: 'Opprett bruker' });
+};
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -7,5 +7,7 @@ const router = Router();
 // routes
 router.get('/', mainController.home_get);
 router.get('/blogger', mainController.blogger_get);
+router.get('/logg-inn', mainController.login_get);
+router.get('/lag-bruker', mainController.create_get);
 
 module.exports = router;
